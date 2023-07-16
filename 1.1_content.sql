0c8ded942c14d8ad7b50c5419e8fe21a6b9dbee5
ChandraMohan-Sah <97285843+ChandraMohan-Sah@users.noreply.github.com>
2023-07-16 10:21:34 +0545

    Update 1.1_content.sql

--- 1.1_content.sql
+++ 1.1_content.sql
@@ -1,1 +1,87 @@
 
+--ALL ABOUT SQL
+
+1) -- Understand CONSTRAINTS in SQL ;
+
+2) --CREATION of Database , Table ;
+
+3) --INSERTION in Table (See NOTECOPY) ;
+'
+	2 ways:
+		WITHOUT Specifying Attribute
+		
+		By Specifying Attribute
+
+'
+
+
+4) --DELETE and UPDATE  operation in Table; (See NOTECOPY)
+'	
+	DELETE OPERATION
+		-- Rows in a Table
+		-- All Rows in a Table
+		-- Entire Table (DROP) 
+			|--> (Both data and Table)
+	
+	UPDATE OPERATION
+		--Used for Updating data inside table		
+	
+	UNDERSTAND REFERENTIAL Triggered Action	While Updating OR Deleting
+	
+'
+
+5) --ALTER operation (Applied For Columns in Table); (See NOTECOPY)
+'
+	GENERALLY 5 OPERATIONS ARE DONE
+	
+	1) ADD Column
+	2) DELETE Column/DROP
+	3) Modify Column
+	
+	4) ADD constraints  |--> ON existing Columns
+	5) DROP constraints |
+'
+
+6) --SELECT Operation (See NOTECOPY) ;
+
+7) --view Operation
+
+8) --Aliasing (AS)
+
+9) --Pattern Matching (Like, Not Like)
+
+10) --SET operation (UNION, INTERSECT, EXCEPT)
+
+11) --Arithmetic Operation (> , < , = , AND MANY MORE)
+
+12) --ORDER BY (ASC, DSC)
+
+13) --IN OPERATOR and Understanding SubQUERIES
+
+14) --ANY/SOME , ALL
+
+15) --EXISTS ,NOT EXISTS
+
+16) --AGGREGATE fuNCTION 
+
+17) --Dealing with NULL Values in aggregate FUNCTION (not Read)
+
+18) --GROUP BY
+
+19) --WITH CLAUSE
+
+20) -- JOINS
+'
+	INNER JOIN
+	
+	LEFT JOIN
+	
+	RIGHT JOIN
+	
+	CROSS JOIN
+	
+	SELF JOIN
+'
+	
+	-- PRACTICE IS WHAT MAKES BEST ,DIFFERENT FROM REST	
+	
